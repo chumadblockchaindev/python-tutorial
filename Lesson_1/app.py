f7f0e3541bbc4b8c63154fe37423c7b72c82bf94
Chukwuma Ezenobi <chukwumaizu@icloud.com>
2025-06-11 12:17:38 +0100

    first commit

--- Lesson_1/app.py
+++ Lesson_1/app.py
@@ -1,31 +1,66 @@
 # First python tutorial
 
-# print("Hello World!!")
-# print("Python Programming\n")
-# print("Chukwuma Obi")
-# print("Osas is in class today. Hurrayyyyy!!")
+# print("Hello, World!")
 
-# Variables
-# age = 27
-# name = "Obi"
+# Variables and Assignment Operators
 
-# name = input("Enter your name:")
-# age = input("Enter your age:")
+# from math import sqrt
+# name = "John"
+# age = 30
+# income = 50000.50
+# message = "Hello World"
+# is_food_availabe = True
 
-# print(name, "is he's name. He is", age, "years old")
+# print(name)
+# print(age)
+# print(income)
+# print(message)
+# print(is_food_availabe)
+# The variable is 'name' in the above code
 
-# Data Types
+# Commenting in python
+# This is a single line comment
+"""This is a doc string comment"""
 
-# print("*" * 100)
-# first_name = "Chukwuma"
-# last_name = "Ezenobi"
-# age = 50
-# income = 2500.45
-# light_on = True
-# tv_on = False
-# print("Welcome " + first_name + " " + last_name)
-# print(f"{first_name} {last_name}")
-# print(type(first_name))
-# print(type(age))
-# print(type(income))
-# print(type(light_on))
+# Maths Operators in Python
+
+# print(5+3)
+# print(6-8)
+# print(9*4)
+# print(6/2)
+# print(10 % 7)
+# from math import sqrt
+
+# number_one = 67
+# number_two = 47
+# add_result = number_one + number_two
+
+# print("The result of adding", number_one,
+#       " and ",  number_two, " is ", add_result)
+# # print(sqrt(64))
+# print(round(45.8546, 3))
+
+# Data Types and Convertion in Python
+name = "Chukwuma"
+age = 30
+income = 50000.50
+is_food_availabe = True
+
+# print("Data Type of ", name, " is ",  type(name))
+# print("Data Type: ", type(age))
+# print("Data Type: ", type(income))
+# print("Data Type: ", type(is_food_availabe))
+# print(name)
+#  Converting from one Data type to another
+# Age is currently an integer
+age = 30
+print(type(age))
+# Converting age to a string
+age_as_string = str(age)
+print(type(age_as_string))
+
+birth_day = "14"
+print(type(birth_day))
+birthday_as_int = bool(birth_day)
+print(type(birthday_as_int))
+print(birthday_as_int)
